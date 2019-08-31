25f1bc939f7735bf568a2f37b0e3eb8afb30f6d4
Grant Burry <burryg@vmware.com>
2019-08-30 23:36:09 -0600

    Build toolset, copyright updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,20 @@
+/*
+ * This file is part of certish. © 2019 certish.
+ *
+ * certish is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * certish is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with certish. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import React from 'react';
 import App from 'next/app';
 import { Helmet } from 'react-helmet';
